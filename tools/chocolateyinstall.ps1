ee519cfd5da12b8c011d9796aa129442ef83eae6
Érico Vieira Porto <ericoporto2008@gmail.com>
2019-03-24 06:48:31 -0300

    Update chocolateyinstall.ps1
    Do server features before

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -25,7 +25,7 @@ $packageArgs = @{
 
 if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
   if((wmic os get caption | Out-String).Contains("Server")) {
-    $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
+    $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
     $statements = "cmd.exe $packageArgs"
     Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
   }
