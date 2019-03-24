846be3ea48a3ffc53ecc88097550b5e490735b99
ericoporto <ericoporto2008@gmail.com>
2019-03-24 18:03:32 -0300

    uses dependencies to pull needed dotnet versions instead of downloading on the install.ps1 script

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -23,32 +23,4 @@ $packageArgs = @{
   silentArgs   = '/VERYSILENT /MERGETASKS="!desktopicon" /SUPPRESSMSGBOXES /NORESTART /SP-' 
 }
 
-if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-  if((wmic os get caption | Out-String).Contains("Server")) {
-    $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
-    $statements = "cmd.exe $packageArgs"
-    Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
-  }
-  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
-  $statements = "cmd.exe $packageArgs"
-  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
-}
-else {
-  Write-Host "Microsoft .Net 3.5 Framework is already installed on your machine."
-} 
-
-if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4.0")) {
-  if((wmic os get caption | Out-String).Contains("Server")) {
-    $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx4"
-    $statements = "cmd.exe $packageArgs"
-    Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
-  }
-  $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx4"
-  $statements = "cmd.exe $packageArgs"
-  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
-}
-else {
-  Write-Host "Microsoft .Net 4.0 Framework is already installed on your machine."
-} 
-
 Install-ChocolateyPackage @packageArgs 
