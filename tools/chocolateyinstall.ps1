64e7dcc3a33e29bd2e5abaf2ada7342cb27cd790
ericoporto <ericoporto2008@gmail.com>
2019-03-25 17:27:43 -0700

    remove unneeded uninstall script, removed 64bit urls, add Release brought by to nuspec release notes

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,21 +3,17 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url        = 'https://github.com/adventuregamestudio/ags/releases/download/v.3.4.3.1/AGS-3.4.3-P1.exe' 
-$url64      = 'https://github.com/adventuregamestudio/ags/releases/download/v.3.4.3.1/AGS-3.4.3-P1.exe' 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'Adventure Game Studio 3.4.3' 
   
   checksum      = '89ef0fcb9ef460352039d2bb589eff31'
   checksumType  = 'md5'
-  checksum64    = '89ef0fcb9ef460352039d2bb589eff31'
-  checksumType64= 'md5'
 
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/VERYSILENT /MERGETASKS="!desktopicon" /SUPPRESSMSGBOXES /NORESTART /SP-' 
