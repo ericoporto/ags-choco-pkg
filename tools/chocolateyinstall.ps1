558f6ac4668988f7e84d6dd69a09052fff92412c
e <ericoporto2008@gmail.com>
2021-12-31 11:15:25 -0300

    update to AGSS 3.5.1.8

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://github.com/adventuregamestudio/ags/releases/download/v.3.5.1.7/AGS-3.5.1.7.exe' 
+$url        = 'https://github.com/adventuregamestudio/ags/releases/download/v.3.5.1.8/AGS-3.5.1.8.exe' 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,7 +12,7 @@ $packageArgs = @{
 
   softwareName  = 'Adventure Game Studio 3.5.1' 
   
-  checksum      = '65dc038b976ffc9fb6e48ba67cb224be'
+  checksum      = '59E5902C2EF9D2E258D14356468CEE71'
   checksumType  = 'md5'
 
   validExitCodes= @(0, 3010, 1641)
